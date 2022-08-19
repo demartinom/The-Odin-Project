350629c8545b627e42f25d067c98cedddf8257cc
“Matt <matt.s.demartino@gmail.com>
2022-08-19 13:51:53 -0400

    Add win variables

--- Rock-Paper-Scissors/script.js
+++ Rock-Paper-Scissors/script.js
@@ -1,28 +1,31 @@
 function getComputerChoice() {
-    let choices = ["rock", "paper", "scissors"];
-    let computerChoice = choices[Math.floor(Math.random() * 3)];
-    return computerChoice
+  let choices = ["rock", "paper", "scissors"];
+  let computerChoice = choices[Math.floor(Math.random() * 3)];
+  return computerChoice;
+}
+
+function playRound() {
+  let playerSelection = prompt("what do you choose?").toLowerCase();
+  let computerSelection = getComputerChoice(); 
+  let playerWin = `You win! ${playerSelection} beats ${computerSelection}`
+  let computerWin = `You lose! ${computerSelection} beats ${playerSelection}`
+  if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "rock")
+  ) {
+    return playerWin;
+  } else if (
+    (playerSelection === "rock" && computerSelection === "paper") ||
+    (playerSelection === "scissors" && computerSelection === "rock") ||
+    (playerSelection === "paper" && computerSelection === "scissors")
+  ) {
+    return computerWin;
+  } else if (playerSelection === computerSelection) {
+    return "You tied!";
   }
-  
-  function playRound() {
-    let playerSelection = prompt('what do you choose?').toLowerCase()
-    let computerSelection = getComputerChoice()
-    if (
-      (playerSelection === "rock" && computerSelection === "scissors") ||
-      (playerSelection === "scissors" && computerSelection === "paper") ||
-      (playerSelection === "paper" && computerSelection === "rock")
-    ) {
-      return `You win! ${playerSelection} beats ${computerSelection}`;
-    } else if (
-      (playerSelection === "rock" && computerSelection === "paper") ||
-      (playerSelection === "scissors" && computerSelection === "rock") ||
-      (playerSelection === "paper" && computerSelection === "scissors")
-    ) {
-      return `You lose! ${computerSelection} beats ${playerSelection}`;
-    } else if (playerSelection === computerSelection) {
-      return "You tied!";
-    }
-  }
-  
-  console.log(playRound());
-  
+}
+function playGame() {
+  let playerScore = 0;
+  let computerScore = 0;
+}
