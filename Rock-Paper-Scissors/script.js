56e088cd3e919e7bf5ebab469c9ec424133cc236
“Matt <matt.s.demartino@gmail.com>
2022-08-19 14:04:39 -0400

    Add scoring

--- Rock-Paper-Scissors/script.js
+++ Rock-Paper-Scissors/script.js
@@ -3,29 +3,36 @@ function getComputerChoice() {
   let computerChoice = choices[Math.floor(Math.random() * 3)];
   return computerChoice;
 }
+let playerScore = 0;
+let computerScore = 0;
 
 function playRound() {
   let playerSelection = prompt("what do you choose?").toLowerCase();
-  let computerSelection = getComputerChoice(); 
-  let playerWin = `You win! ${playerSelection} beats ${computerSelection}`
-  let computerWin = `You lose! ${computerSelection} beats ${playerSelection}`
+  let computerSelection = getComputerChoice();
+  let playerWin = `You win! ${playerSelection} beats ${computerSelection}`;
+  let computerWin = `You lose! ${computerSelection} beats ${playerSelection}`;
   if (
     (playerSelection === "rock" && computerSelection === "scissors") ||
     (playerSelection === "scissors" && computerSelection === "paper") ||
     (playerSelection === "paper" && computerSelection === "rock")
   ) {
+    playerScore++;
     return playerWin;
   } else if (
     (playerSelection === "rock" && computerSelection === "paper") ||
     (playerSelection === "scissors" && computerSelection === "rock") ||
     (playerSelection === "paper" && computerSelection === "scissors")
   ) {
+    computerScore++;
     return computerWin;
   } else if (playerSelection === computerSelection) {
     return "You tied!";
   }
 }
+
 function playGame() {
-  let playerScore = 0;
-  let computerScore = 0;
+  for (let i = 0; i < 5; i++) {
+    console.log(playRound());
+  }
 }
+playGame();
