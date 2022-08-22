a66711e5459da805a372a7406a2d5702a34612b6
“Matt <matt.s.demartino@gmail.com>
2022-08-22 15:10:37 -0400

    Change grid button complete

--- Etch-A-Sketch/script.js
+++ Etch-A-Sketch/script.js
@@ -11,12 +11,14 @@ const windowFunctions = () => {
   gridButton.addEventListener("click", changeGrid);
 
   const createGrid = (gridSize) => {
-    gridContainer.innerHTML = ''
+    gridContainer.innerHTML = "";
     for (let i = 0; i < gridSize * gridSize; i++) {
       let gridBox = document.createElement("div");
       gridBox.setAttribute("class", "grid-box");
       gridContainer.appendChild(gridBox);
     }
+    gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 40px)`;
+    gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 40px)`;
   };
 
   createGrid(defaultSize);
