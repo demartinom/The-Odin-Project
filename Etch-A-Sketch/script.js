ba378131ca7f9cb583e445ec0e206914071b2d7a
“Matt <matt.s.demartino@gmail.com>
2022-08-22 14:58:42 -0400

    complete reset grid button

--- Etch-A-Sketch/script.js
+++ Etch-A-Sketch/script.js
@@ -1,11 +1,24 @@
-let gridContainer = document.querySelector(".grid-container");
+const windowFunctions = () => {
+  let gridContainer = document.querySelector(".grid-container");
+  let gridButton = document.querySelector(".prompt");
+  let defaultSize = 16;
+  function changeGrid() {
+    let sizeChoice = prompt(
+      "How big a grid would you like? It cannot be large than 100x100"
+    );
+    createGrid(sizeChoice);
+  }
+  gridButton.addEventListener("click", changeGrid);
 
-const createGrid = () => {
-    for (let i = 0; i < 256; i++){
-        let gridBox = document.createElement('div')
-        gridBox.setAttribute('class', 'grid-box')
-        gridContainer.appendChild(gridBox)
+  const createGrid = (gridSize) => {
+    gridContainer.innerHTML = ''
+    for (let i = 0; i < gridSize * gridSize; i++) {
+      let gridBox = document.createElement("div");
+      gridBox.setAttribute("class", "grid-box");
+      gridContainer.appendChild(gridBox);
     }
-};
+  };
 
-createGrid()
+  createGrid(defaultSize);
+};
+window.onload = windowFunctions;
