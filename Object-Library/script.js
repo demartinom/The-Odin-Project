71baafa284923850cd13048c4c9251c3f373fca4
“Matt <matt.s.demartino@gmail.com>
2022-08-23 16:09:42 -0400

    Attempt to add toggle button

--- Object-Library/script.js
+++ Object-Library/script.js
@@ -5,7 +5,7 @@
 // };
 let libContainer = document.querySelector(".library");
 let newBookButton = document.querySelector(".new-book");
-let bookForm = document.querySelector('.add-book')
+let bookForm = document.querySelector(".add-book");
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -26,20 +26,42 @@ const displayLibrary = () => {
 };
 
 const newBookForm = () => {
+  bookForm.innerHTML = `<input type="text" id='title' placeholder="Book Title" />
+  <input type="text" id='author' placeholder="Author" />
+  <input type="text" id='pages' placeholder="# of pages" />
+  <input type="text" id='read' placeholder="read?" />
+  <button type="submit">Add Book</button>`;
 };
 
 newBookButton.addEventListener("click", newBookForm);
-
+bookForm.addEventListener("submit", function (e) {
+  e.preventDefault;
+});
 const createCard = (book) => {
   let newCard = document.createElement("div");
   newCard.setAttribute("class", "book-card");
-  newCard.innerHTML = `Name: ${book.title} <br> Author: ${book.author} <br> # of pages: ${book.pages} <br> Have I read it?: ${book.read}`;
+  newCard.innerHTML = `Name: ${book.title} <br> Author: ${book.author} <br> # of pages: ${book.pages} <br> Have I read it?: ${book.read} `;
   libContainer.appendChild(newCard);
+  let readToggle = document.createElement("input");
+  readToggle.setAttribute("type", "checkbox");
+  readToggle.setAttribute("class", "read-toggle");
+
+  newCard.appendChild(readToggle);
   let deletebutton = document.createElement("button");
   deletebutton.innerText = "Delete Book";
   newCard.appendChild(deletebutton);
   deletebutton.addEventListener("click", function () {
     newCard.remove();
+    let readToggle = document.querySelectorAll(".read-toggle");
+    readToggle.forEach((click) => {
+      click.addEventListener("click", function () {
+        if ((click.checked = true)) {
+          book.read = "read";
+        } else {
+          book.read = "not read";
+        }
+      });
+    });
   });
 };
 
