482b9c3d0dae61ec01556ccd16359217fee4ba05
“Matt <matt.s.demartino@gmail.com>
2022-08-23 14:17:18 -0400

    Initial Commit

---
+++ Object-Library/script.js
@@ -0,0 +1,14 @@
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+Book.prototype = {
+  info() {
+    return `${this.title} by ${this.author}, ${this.pages}, ${this.read}`;
+  },
+};
+
+
