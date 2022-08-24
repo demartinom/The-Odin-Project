7c9c42e760a79513ff38621007eae4fc8bdccc68
“Matt <matt.s.demartino@gmail.com>
2022-08-24 12:38:20 -0400

    Add click event listener

--- Tic-Tac-Toe/script.js
+++ Tic-Tac-Toe/script.js
@@ -9,3 +9,21 @@ const gameBoard = (() => {
   });
 })();
 
+const player = (name, tile) => {
+  const playerName = () => name;
+  const playerTile = () => tile;
+  return { playerName, playerTile };
+};
+const player1 = player("Matt", "X");
+const boardSquare = document.querySelectorAll(".square");
+
+function clickEvent(event) {
+  let squareValue = document.createElement("p");
+  squareValue.innerText = player1.playerTile();
+  event.appendChild(squareValue);
+}
+boardSquare.forEach((square) => {
+  square.addEventListener("click", function () {
+    clickEvent(square);
+  });
+});
