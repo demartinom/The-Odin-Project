2bc0ad7d0b8ed95c5690a9fe7f2758344f28c3ea
“Matt <matt.s.demartino@gmail.com>
2022-08-25 16:29:36 -0400

    Create todo object

---
+++ Todo-List/script/todoInfo.js
@@ -0,0 +1,8 @@
+export default class toDo {
+  constructor(name, description, dueDate, priority) {
+    this.name = name;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+  }
+}
