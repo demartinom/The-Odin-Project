311a1efbceb17673af50cd52e2625e1af7a8eff9
“Matt <matt.s.demartino@gmail.com>
2022-08-25 16:29:00 -0400

    Began creating main

---
+++ Todo-List/script/main.js
@@ -0,0 +1,7 @@
+import toDo from "./todoInfo.js"; //keys = name, description, duedate, priority
+import createCard from "./createCard.js";
+
+let Matt = new toDo('matt', 'dfdf', 'dfdfd', 'dfdfdf')
+
+
+createCard(Matt);
