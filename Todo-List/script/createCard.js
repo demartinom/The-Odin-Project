619d11f8fb5f4780c6f0c441490b1d906b5745de
“Matt <matt.s.demartino@gmail.com>
2022-08-25 16:31:11 -0400

    Add classes to text

--- Todo-List/script/createCard.js
+++ Todo-List/script/createCard.js
@@ -2,18 +2,20 @@ export default function createCard(object) {
   const container = document.createElement("div");
   container.setAttribute("class", "toDoCard");
   toDoTitle(container, object.name);
-  toDoDescription(container, object.description)
+  toDoDescription(container, object.description);
   document.body.appendChild(container);
 }
 
 const toDoTitle = (div, name) => {
   const title = document.createElement("h2");
   title.innerText = name;
+  title.setAttribute("class", "todo-title");
   div.appendChild(title);
 };
 
 const toDoDescription = (div, title) => {
   const description = document.createElement("p");
-  description.innerText = title
-  div.appendChild(description)
+  description.innerText = title;
+  description.setAttribute("class", "todo-description");
+  div.appendChild(description);
 };
